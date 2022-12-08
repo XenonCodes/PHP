e269e24a872338bbcbf62c34fa46800cc0a8617b
Xenon <SDA152nn@gmail.com>
2022-12-08 03:24:32 +0300

    Added a constant

--- index.php
+++ index.php
@@ -1,12 +1,13 @@
 <?php
     //input data
     $nameUser = readline("Введите Ваше Имя: ");
+    define("ASK_TIME", "Сколько примерно времени эта задача займет? ");
     $task1 = readline("Какая первая задача стоит перед вами сегодня? ");
-    $time1 = readline("Сколько примерно времени эта задача займет? ");
+    $time1 = readline(ASK_TIME);
     $task2 = readline("Какая вторая задача стоит перед вами сегодня? ");
-    $time2 = readline("Сколько примерно времени эта задача займет? ");
+    $time2 = readline(ASK_TIME);
     $task3 = readline("Какая третья задача стоит перед вами сегодня? ");
-    $time3 = readline("Сколько примерно времени эта задача займет? ");
+    $time3 = readline(ASK_TIME);
 
     //main logic
     $totalTime = $time1 + $time2 + $time3;
